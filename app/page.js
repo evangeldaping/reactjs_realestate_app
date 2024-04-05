16d908b9eae06769e78e1dddee36316052bc694a
Evangel Daping <evangeldaping@gmail.com>
2024-04-05 14:57:16 +0100

    Home props
    Add properties and flex component to fetch properties and map over them. Add async for function to fetchApi

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Flex, Box, Text, Button } from '@chakra-ui/react'
+import { Flex, Box, Text, Button } from '@chakra-ui/react';
+
+import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
@@ -16,7 +18,7 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
   </Flex>
 )
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) { 
   return (
     <Box>
       <Banner
@@ -29,6 +31,10 @@ export default function Home() {
         linkName='/search?purpose=for-rent'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
       />
+      <Flex flexWrap='wrap'>
+        {/* Fetch the properties and map over them */}
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
       <Banner
         purpose='BUY A HOME'
         title1=' Find, Buy & Own Your'
@@ -39,6 +45,22 @@ export default function Home() {
         linkName='/search?purpose=for-sale'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
+      <Flex flexWrap='wrap'>
+        {/* Fetch the properties and map over them */}
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
     </Box>
   );
+}
+
+async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
